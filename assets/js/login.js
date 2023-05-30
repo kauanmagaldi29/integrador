058a41a2505d9de0ecc6f245583a1a644a8579f6
kauanmagaldii <kauanmagaldi29@gmail.com>
2023-05-30 14:06:42 -0300

    alteracao

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,7 +8,7 @@ function validateForm(event) {
     // Verifica se o usuário e senha estão corretos
     if (username === "sabrina" && password === "123") {
       // Redireciona para a outra página
-      window.location.href = "https://lookerstudio.google.com/s/ucO6Ll0Y5Uw";
+      window.location.href = "https://lookerstudio.google.com/reporting/6adc297d-587c-4500-afe5-0cfb096b788a";
     } else {
       var errorText = document.getElementById("errorText");
       errorText.textContent = "Usuário ou senha inválidos.";
